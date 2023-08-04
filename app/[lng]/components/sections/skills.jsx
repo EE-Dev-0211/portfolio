a85e9d8731404ffd4d7c9061ef02175700ee61f5
Eric Eiselt <with.hearts.toward.none@protonmail.com>
2023-08-04 08:52:45 +0200

    test

--- app/[lng]/components/sections/skills.jsx
+++ app/[lng]/components/sections/skills.jsx
@@ -35,7 +35,7 @@ export default function Skills({ t }) {
        md:gap-16 md:flex-row md:items-start md:mx-10"
       >
         <div
-          className=" items-center bg-gray-100 dark:bg-black text-center
+          className="items-center bg-gray-100 dark:bg-black text-center
     p-6 md:pt-8 md:px-8 md:pb-4 rounded-xl mt-6 mb-10 w-60 md:w-1/4 flex flex-col
     shadow-lg border-solid dark:border-double border-2 dark:border-teal-200 border-gray-800
     bg-opacity-10 backdrop-filter backdrop-blur-sm  dark:bg-opacity-10  dark:backdrop-filter
@@ -188,12 +188,6 @@ export default function Skills({ t }) {
                           onMouseEnter: () => setTooltipContent(framLib.name),
                           onMouseLeave: () => setTooltipContent(null),
                         })}
-                        {/*{tooltipContent === framLib.name && (*/}
-                        {/*  <Tooltip*/}
-                        {/*    tooltipKey={tooltipContent}*/}
-                        {/*    tooltipContent={framLib.name}*/}
-                        {/*  />*/}
-                        {/*)}*/}
                       </div>
                     </div>
                     <div className="col-span-2">
